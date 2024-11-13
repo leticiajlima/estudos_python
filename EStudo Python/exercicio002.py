edf4258695767a686d36207cf29ef7ea1c5c4c5d
Leticia Lima <152423942+leticiajlima@users.noreply.github.com>
2024-11-13 17:14:41 -0300

    Add files via upload

---
+++ EStudo Python/exercicio002.py
@@ -0,0 +1,5 @@
+nome = input('Qual o seu nome? ')
+idade = int(input('Qual a sua idade? '))  # Convertendo para inteiro
+peso = float(input('Qual o seu peso? '))  # Convertendo para float
+
+print(nome, idade, peso)
---
+++ EStudo Python/exercicio003.py
@@ -0,0 +1,2 @@
+nome = input('Qual o seu nome? ')
+print('Bem vindo {}!'.format(nome))
